8477d6a7d37fb3b2056593ecbbad6713e93bb5d8
Daryl Findlay <darylfindlay@gmail.com>
2019-06-12 22:58:09 +0100

    add sequelize-cli with user model

---
+++ seeders/20190612214131-demo-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'demo@demo.com',
+      password: '0987654321234567890',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
